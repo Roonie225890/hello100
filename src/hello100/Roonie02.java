bd04736c4f1b2312f6eaac4c451db240f5bf1f12
Roonie225890 <wei225890@hotmail.com>
2016-08-14 15:25:57 +0800

    byte + short = int

---
+++ src/hello100/Roonie02.java
@@ -0,0 +1,12 @@
+package hello100;
+
+public class Roonie02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 10, b = 0;
+		int c = a / b;
+		System.out.println(c);
+	}
+
+}
---
+++ src/hello100/Roonie03.java
@@ -0,0 +1,19 @@
+package hello100;
+
+public class Roonie03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// char: 2^16 => 0 - 65535
+		char v1 = 'a';
+		char v2 = 98;  // ASCII: b
+		char v3 = '資';
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v3);
+		
+		int v4 = v1 + v2;
+		System.out.println(v4);
+	}
+
+}
