9f73947a8d700faa6d7850f14c4472b34491443a
Roonie225890 <wei225890@hotmail.com>
2016-08-20 10:18:04 +0800

    08

---
+++ src/hello100/Roonie08.java
@@ -0,0 +1,21 @@
+package hello100;
+
+public class Roonie08 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 3;
+		
+		int c = a & b;
+		System.out.println(c);
+		//--a;
+//		System.out.println(a);
+//		System.out.println(a--);
+//		System.out.println(a);
+//		if (--a >= 10 & b++ <= 3){
+//			System.out.println("OK:a=" + a + ",b=" + b);
+//		}else{
+//			System.out.println("XX:a=" + a + ",b=" + b);
+//		}
+	}
+
+}
