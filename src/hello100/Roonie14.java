c6b8c547e42c59eb9853543f123793103b4672c8
Roonie225890 <wei225890@hotmail.com>
2016-08-20 14:37:25 +0800

    14

---
+++ src/hello100/Roonie14.java
@@ -0,0 +1,35 @@
+package hello100;
+
+public class Roonie14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for (int i=1; i<=100; i++){
+//		判斷是否為質數
+//			System.out.print(i + "  ");
+//			if (i % 10 == 0 ){
+//				System.out.println();
+//			}
+//		}
+		
+		for (int j = 0; j < 10; j++) {
+			for (int i = 1; i <=10; i++) {
+				int v = j * 10 + i;
+				// 判斷是否為質數
+				boolean isOK = true;
+				for (int k=2; k<=v/2; k++) {
+				//for (int k = 2; k<v; k++) {
+					if (v % k == 0) {
+						//你不是質數
+						isOK = false;
+						break;
+					}
+				}
+				
+				System.out.print(v + (isOK?"*":" ")  + "  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
