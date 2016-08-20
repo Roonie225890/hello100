3ebe40562c32c3497506e7ae45da4a993e8bc61d
Roonie225890 <wei225890@hotmail.com>
2016-08-20 14:00:19 +0800

    9x9

--- src/hello100/Roonie9x9.java
+++ src/hello100/Roonie9x9.java
@@ -3,7 +3,7 @@ package hello100;
 public class Roonie9x9 {
 
 	public static void main(String[] args) {
-		for (int k = 0; k < 5; k++) {
+		for (int k = 0; k < 4; k++) {
 			for (int j = 1; j <= 9; j++) {
 				for (int i = 2; i <= 5; i++) {
 					int newi = i + k * 4;
