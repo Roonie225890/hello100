933c116873719e227892c0644b0b4bc5a5ae8d1d
Roonie225890 <wei225890@hotmail.com>
2016-08-20 12:12:31 +0800

    12

---
+++ src/hello100/Roonie12.java
@@ -0,0 +1,15 @@
+package hello100;
+
+public class Roonie12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		for (System.out.println("Roonie"); ;System.out.println("------")){
+			System.out.println(i++);
+			
+		}
+		//System.out.println("Game Over");
+	}
+
+}
