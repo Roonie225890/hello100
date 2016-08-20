f7dfd38341ac668100043938746b5aa183dd639a
Roonie225890 <wei225890@hotmail.com>
2016-08-20 09:32:48 +0800

    07p3

---
+++ src/hello100/Roonie07p3.java
@@ -0,0 +1,22 @@
+package hello100;
+
+public class Roonie07p3 {
+
+	public static void main(String[] args) {
+		int year = 2016;
+		if (year % 4 == 0){
+			if (year % 100 == 0){
+				if (year % 400 == 0){
+					//29
+				}else{
+					//28
+				}
+			}else{
+				//29
+			}
+		}else{
+			//28
+		}
+	}
+
+}
