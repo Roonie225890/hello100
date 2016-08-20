2bc7acd2cd782a8a30fb2a6447aced8da1dda4c2
Roonie225890 <wei225890@hotmail.com>
2016-08-20 11:04:37 +0800

    09

---
+++ src/hello100/Roonie09.java
@@ -0,0 +1,25 @@
+package hello100;
+
+public class Roonie09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dir = 10;
+		final int b = 9;
+		switch(dir){
+		case 1:
+			System.out.println("A");
+			break;
+		case 10:
+			System.out.println("B1");
+			break;
+		case b+2:
+			System.out.println("B2");
+			break;
+		
+		default:
+			System.out.println("X");
+		}
+	}
+
+}
