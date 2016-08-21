6947649f8c29c06970e8ed597cb9ef92a0fe5d14
Roonie225890 <wei225890@hotmail.com>
2016-08-21 13:16:45 +0800

    23

---
+++ src/hello100/Roonie23.java
@@ -0,0 +1,21 @@
+package hello100;
+
+public class Roonie23 {
+
+	public static void main(String[] args) {
+		int i=0, sum = 0, n = 947;
+//		while (i<10){
+//			System.out.println(i++);
+//		}
+//		for (;i<10;){
+//			System.out.println(i++);
+//		}
+		while (i<=n){
+			sum += i++;
+		}
+			System.out.println("1 + 2 +....+n" + " = " + sum);
+			
+		}
+	}
+
+
