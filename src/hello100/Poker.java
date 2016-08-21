3049fbabc7dabf96353e5f06f1aa3c69aade8d5b
Roonie225890 <wei225890@hotmail.com>
2016-08-21 10:38:51 +0800

    poker01

---
+++ src/hello100/Poker.java
@@ -0,0 +1,31 @@
+package hello100;
+
+public class Poker {
+
+	public static void main(String[] args) {
+		// 洗牌
+		int[] poker = new int[52]; //0,0,....0
+		for (int i=0; i<poker.length; i++){
+			int rand = (int)(Math.random()*52);
+			// 檢查機制
+			boolean isOK = true;
+			for (int j=0; j<i; j++){
+				if (poker[j]==rand){
+					isOK = false;
+					break;
+				}
+			}
+			if (!isOK){
+				i--;
+				continue;
+			}
+			poker[i] = rand;
+			System.out.println(poker[i]);
+		}
+		//發牌=> 四個玩家
+		
+		//理牌 => 攤牌
+
+	}
+
+}
