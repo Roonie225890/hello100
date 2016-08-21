b1cf9e242eeb0fcae3c58b35cd938780e6ac6bb4
Roonie225890 <wei225890@hotmail.com>
2016-08-21 09:19:16 +0800

    20

--- src/hello100/Roonie20.java
+++ src/hello100/Roonie20.java
@@ -6,37 +6,9 @@ public class Roonie20 {
 		// TODO Auto-generated method stub
 		int[] p = new int[6];//p[0] = 0,p[1] = 0, p[2] = 0;...p[5] = 0;
 		for (int i=0; i<100000; i++){
-			int point = (int)(Math.random()*9+1);// 1 - 9
-			switch(point){
-			case 1:
-				p[0]++;
-				break;
-			case 2:
-				p[1]++;
-				break;
-			case 3:
-				p[2]++;
-				break;
-			case 4:
-				p[3]++;
-				break;
-			case 5:
-				p[4]++;
-				break;
-				case 6:
-					p[5]++;
-					break;
-			case 7:
-				p[3]++;
-				break;
-			case 8:
-				p[4]++;
-				break;
-			case 9:
-				p[5]++;
-				break;
-			}
-			
+			int point = (int)(Math.random()*9);// 0 - 8
+			p[(point>=6?point-3:point)]++;
+					
 		}
 		for (int i = 0; i<p.length; i++){
 			System.out.println((i+1) + ":" + p[i]);
